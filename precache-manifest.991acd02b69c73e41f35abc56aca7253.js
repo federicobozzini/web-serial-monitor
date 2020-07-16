00d700c6a42ba28894699e2fb42c058da6eed884
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-16 10:00:24 +0000

    Deploy federicobozzini/web-serial-monitor to github.com/federicobozzini/web-serial-monitor.git:gh-pages

---
+++ precache-manifest.991acd02b69c73e41f35abc56aca7253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b36967558bb29fd0c86b645f8571fcb4",
+    "url": "/web-serial-monitor/index.html"
+  },
+  {
+    "revision": "2032a5d5f862fac4a21b",
+    "url": "/web-serial-monitor/static/css/main.f26fb67d.chunk.css"
+  },
+  {
+    "revision": "187f820140d0deb0af49",
+    "url": "/web-serial-monitor/static/js/2.3a5790c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-serial-monitor/static/js/2.3a5790c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2032a5d5f862fac4a21b",
+    "url": "/web-serial-monitor/static/js/main.fab0e934.chunk.js"
+  },
+  {
+    "revision": "a2021a7bcfc818e9142b",
+    "url": "/web-serial-monitor/static/js/runtime-main.902e1185.js"
+  }
+]);
